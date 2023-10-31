92ab762d6278cd6f3a8e89dd7beab7fd61db67ad
SteveA28 <alonsosteven32@gmail.com>
2023-10-30 21:07:39 -0400

    added display timer

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
-const timeDisplayEl = $('#time-display');
-const projectDisplayEl = $('#project-display');
-const projectFormEl = $('#project-form');
-const projectNameInputEl = $('#project-name-input');
-const projectTypeInputEl = $('#project-type-input');
-const projectDateInputEl = $('#project-date-input');
+const timeDisplayEl = $("#time-display");
+const projectDisplayEl = $("#project-display");
+const projectFormEl = $("#project-form");
+const projectNameInputEl = $("#project-name-input");
+const projectTypeInputEl = $("#project-type-input");
+const projectDateInputEl = $("#project-date-input");
+
+function displayTime() {
+  const now = new Date();
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+  const formattedTime = now.toLocaleString("en-US", options);
+  timeDisplayEl.textContent = formattedTime;
+}
